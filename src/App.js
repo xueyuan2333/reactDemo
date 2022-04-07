fadd210665f5a8a59a87114e286652c6d602b289
xueyuan2333 <13342276951@163.com>
2022-04-07 22:05:59 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React,{useEffect,useState} from 'react';
+import Test from './components/test'
+
+function App() {
+  return (
+    <div>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/hooks/testuseReducer.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState, useReducer } from 'react';
+
+export default function Component() {
+    return (
+        <div>
+            123
+        </div>
+    );
+}
+
