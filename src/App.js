31693842ff024eee01b3061357dedecc3ae386ea
xueyuan <13342276951@163.com>
2022-06-12 20:31:53 +0800

    test1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Test from "./components/test";
+import Child2 from "./components/child2/index";
 
 function App() {
   return (
     <div>
       <Test />
+      <Child2 />
     </div>
   );
 }
--- src/components/child2/index.js
+++ src/components/child2/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Component() {
-  return <div>test 分支0.0.3</div>;
+  return <div>child2组件</div>;
 }
